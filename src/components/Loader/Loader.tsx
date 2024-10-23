df3e6b0a5debd12ca823d72fd5b3b82910a957ea
Uliana Kolodii <ulianakolodii@gmail.com>
2024-10-23 23:06:02 +0300

    chore: add test-id for loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,7 +4,7 @@ import "./Loader.css";
 const Loader = () => {
   return (
     <div className="loader-container">
-      <div className="loader" />
+      <div className="loader" data-testid="loader" />
     </div>
   );
 };
