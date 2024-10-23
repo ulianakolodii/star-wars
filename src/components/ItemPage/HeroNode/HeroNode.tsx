d13e48ae62a44e606901243eaa3dd710b2abccda
Uliana Kolodii <ulianakolodii@gmail.com>
2024-10-23 16:56:34 +0300

    style: add heroNode appearance

--- src/components/ItemPage/HeroNode/HeroNode.tsx
+++ src/components/ItemPage/HeroNode/HeroNode.tsx
@@ -1,32 +1,105 @@
-import React, { FC } from "react";
+import React, { FC, useState, useCallback, useEffect } from "react";
 import "./HeroNode.css";
 import { Hero } from "../../../types/types";
 
 interface HeroNodeProps {
-  data: Hero; // Define the expected shape of the data
+  data: Hero;
 }
 
 const HeroNode: FC<HeroNodeProps> = ({ data }) => {
+  const [homeworld, setHomeworld] = useState("");
+  const [species, setSpecies] = useState("");
+
+  const fetchHomeWorld = useCallback(async () => {
+    try {
+      const response = await fetch(
+        `https://sw-api.starnavi.io/planets/${data.homeworld}`
+      );
+      const responseData = await response.json();
+      setHomeworld(responseData.name);
+    } catch (error) {
+      console.error("Failed to fetch heroes:", error);
+    }
+  }, []);
+
+  const fetchSpecies = useCallback(async () => {
+    try {
+      const response = await fetch(
+        `https://sw-api.starnavi.io/species/${data.species}`
+      );
+      const responseData = await response.json();
+      setSpecies(responseData.name);
+    } catch (error) {
+      console.error("Failed to fetch heroes:", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchHomeWorld();
+    fetchSpecies();
+  }, []);
+
   return (
     <div className="hero-node">
       <div className="hero-node__title">{data.name}</div>
-      <div>{data.gender}</div>
-      <div>{data.birth_year}</div>
+      <div className="hero-node__general">{data.gender}</div>
+      <div className="hero-node__general">{data.birth_year}</div>
       <div className="hero-node__divider" />
-      <div>{data.homeworld}</div>
       <div>
-        <div>{data.mass}kg</div>
-        <div>{data.height}cm</div>
+        <span>home:</span>
+        {homeworld}
+      </div>
+      <div className="hero-node__block">
+        <div>
+          <span>mass:</span>
+          {data.mass}kg
+        </div>
+        <div>
+          <span>height:</span>
+          {data.height}cm
+        </div>
+      </div>
+      <div className="hero-node__block">
+        <div>
+          <span>hair:</span>
+          {data.hair_color}
+        </div>
+        <div>
+          <span>eyes:</span>
+          {data.eye_color}
+        </div>
+        <div>
+          <span>skin:</span>
+          {data.skin_color}
+        </div>
       </div>
       <div>
-        <div>{data.hair_color}</div>
-        <div>{data.eye_color}</div>
-        <div>{data.skin_color}</div>
+        <span>species:</span>
+        {species}
       </div>
-      <div>{data.species}</div>
-      <div>
-        <div>{data.created}</div>
-        <div>{data.edited}</div>
+      <div className="hero-node__block hero-node__block-dates">
+        <div>
+          <span>created:</span>
+          {new Date(data.created).toLocaleString("uk-UK", {
+            year: "numeric",
+            month: "numeric",
+            day: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+          })}
+        </div>
+        <div>
+          <span>edited:</span>
+          {new Date(data.edited).toLocaleString("uk-UK", {
+            year: "numeric",
+            month: "numeric",
+            day: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+          })}
+        </div>
       </div>
     </div>
   );
