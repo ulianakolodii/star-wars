d0293318bf143d310e3c8e0c93d9957ae2cc0460
Uliana Kolodii <ulianakolodii@gmail.com>
2024-10-23 13:31:14 +0300

    fix: fix infinite scroll inside heroes context

--- src/components/HeroesContext.tsx
+++ src/components/HeroesContext.tsx
@@ -43,13 +43,14 @@ export const HeroesProvider = ({ children }) => {
 
   const lastElementRef = useCallback(
     (node) => {
-      if (!hasMore || !observerRef.current) return;
+      if (!hasMore) return;
       if (observerRef.current) observerRef.current.disconnect();
       observerRef.current = new IntersectionObserver((entries) => {
         if (entries[0].isIntersecting) {
           setPage((prevPage) => prevPage + 1);
         }
       });
+
       if (node) observerRef.current.observe(node);
     },
     [hasMore]
