65998f5ee975a73e82212867e267ab205e55e61b
Uliana Kolodii <ulianakolodii@gmail.com>
2024-10-23 17:04:16 +0300

    chore: reorganize heroes data structure

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -12,15 +12,22 @@ const ListItem = forwardRef<HTMLLIElement, { hero: Hero }>(({ hero }, ref) => {
         )}
         <div className="hero-name">{hero.name}</div>
         <div className="hero-info">
-          <div>{hero.birth_year}</div>
+          <div>{hero.birth_year !== "unknown" && hero.birth_year}</div>
           <div className="hero-appearance">
-            <div>{hero.height}cm</div>
-            <div>{hero.mass}kg</div>
             <div>
               <span>eyes:</span> {hero.eye_color}
             </div>
+            <div>
+              <span>hair:</span> {hero.hair_color}
+            </div>
+            <div>
+              <span>skin:</span> {hero.skin_color}
+            </div>
           </div>
-          <div>{hero.homeworld}</div>
+          <div>
+            {hero.height !== "unknown" && <div>{hero.height}cm</div>}
+            {hero.mass !== "unknown" && <div>{hero.mass}kg</div>}
+          </div>
           <div>{hero.films.length}</div>
           <div>{hero.starships.length}</div>
         </div>
